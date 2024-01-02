a6e26e004b984e3abf21bef3c83c63d4ef9dbec9
shirotamaki <shiro.tamaki63@gmail.com>
2024-01-02 21:55:39 +0900

    first commit

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{ user.id }}, {{ user.name }}
+
+      </li>
+    </ul>
+      <h1>hoge</h1>
+      <p>{{ users[0].id }}, {{ users[0].address }}</p>
+  </div>
+</template>
+
+<script setup>
+const { data: users, error } = await useFetch('https://jsonplaceholder.typicode.com/users')
+console.log(error.value)
+if (error.value) {
+  throw createError({ statusCode: 404, statusMessage: 'Page not found !!!!!!!!!!'})
+}
+</script>
