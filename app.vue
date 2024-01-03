f93003a5d445999265c46b2baa390e282795506e
shirotamaki <shiro.tamaki63@gmail.com>
2024-01-03 20:53:02 +0900

    セクション8-46

--- app.vue
+++ app.vue
@@ -1,20 +1,13 @@
 <template>
   <div>
-    <ul>
-      <li v-for="user in users" :key="user.id">
-        {{ user.id }}, {{ user.name }}
-
-      </li>
-    </ul>
-      <h1>hoge</h1>
-      <p>{{ users[0].id }}, {{ users[0].address }}</p>
+    <h1>{{ title }}</h1>
+    <button @click="titleState.changeTitle('Hello Nuxt3!')">
+      changeTitle
+    </button>
   </div>
 </template>
 
 <script setup>
-const { data: users, error } = await useFetch('https://jsonplaceholder.typicode.com/users')
-console.log(error.value)
-if (error.value) {
-  throw createError({ statusCode: 404, statusMessage: 'Page not found !!!!!!!!!!'})
-}
+const titleState = useTitle()
+const { title } = titleState
 </script>
